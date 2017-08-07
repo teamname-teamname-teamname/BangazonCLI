c24d90d273f985e0fbf34853d1cc73ced1d055d7
Joey Biotti <joey@MacBook-Pro-2.local>
2017-08-07 14:23:17 -0500

    added Product.cs model

---
+++ src/Bangazon/Models/Product.cs
@@ -0,0 +1,27 @@
+namespace Bangazon.Models
+{
+    /**
+ * Class: Product
+ * Purpose: The Product class is used to store all product information
+ * Author: Teamname-Teamname-Teamname
+ * Properties:
+ *   ProductId: A unique idetification number for each product
+     ProductTypeId: The unique identification number of the product type associated with this product
+     Product Type: The name of the product type
+     Price: The price of the specified product
+     Title: The name of the specified product
+     Description: A short description of the specified product
+     CustomerId: The unique identification number for the customer
+     Customer: The customer information
+     OrderProducts: A collection of OrderProducts to allow database traversing
+ */
+    public class Product
+    {
+       public int Id {get; set;}
+       public string Name {get; set;}
+       public int CustomerId {get; set;}
+       public int Price {get; set;}
+       public int Quantity {get; set;}
+       public string Description {get; set;}
+    }
+}
