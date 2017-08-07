6d29b7e6bcce2c164bccab91b8ff88b02340eb80
cleverbek1991 <dilshod@Dilshods-MacBook-Pro.local>
2017-08-07 11:53:50 -0500

    customer model and CustomerManager test addedd

--- test/Bangazon.Tests/Bangazon_CustomerManagerShould.cs
+++ test/Bangazon.Tests/Bangazon_CustomerManagerShould.cs
@@ -1,20 +1,37 @@
 using System;
+using Bangazon.Models;
 using Xunit;
 
 namespace Bangazon.Tests
 {
-    public class CustomerManagerShould
+    public class CustomerManagerShould: IDisposable
     {
+        private readonly CustomerManager _cm;
+        private readonly DatabaseInterface _db;
+        public CustomerManagerShould()
+        {
+            _db = new DatabaseInterface("BANGAZONCLI_TEST_DB");
+            _cm = new CustomerManager(_db);
+            _db.CheckCustomerTable();
+        }
         [Fact]
         public void AddNewCustomer()
         {
+            Customer newCustomer = new Customer();
 
+            var result = _cm.AddCustomer(newCustomer);
+            Assert.True(result != 0);
         }
 
         [Fact]
         public void ListCustomers()
         {
 
+        }
+
+        public void Dispose()
+        {
+            // _db.Delete("DELETE FROM customer");
         }
     }
 }
