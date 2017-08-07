fb7a3683c47f512780664fbef3894095b89ae55b
cleverbek1991 <dilshod@Dilshods-MacBook-Pro.local>
2017-08-07 10:54:45 -0500

    customer Model added

---
+++ src/Bangazon/Models/Customer.cs
@@ -0,0 +1,15 @@
+namespace Bangazon.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string phoneNumber { get; set; }
+
+    }
+}
