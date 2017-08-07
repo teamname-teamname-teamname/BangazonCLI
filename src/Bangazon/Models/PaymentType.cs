86070a70347d744d7fbfc710e7b04fe7efda35f4
ollieOsh <sosinusi@gmail.com>
2017-08-07 14:21:55 -0500

    added PaymentType model

---
+++ src/Bangazon/Models/PaymentType.cs
@@ -0,0 +1,10 @@
+namespace Bangazon
+{
+    public class PaymentType
+    {
+        public int id {get; set;}
+        public int AccountNumber {get; set;}
+        public string Name {get; set;}
+        public int CustomerId {get; set;}
+    }
+}
