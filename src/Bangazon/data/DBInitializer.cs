b33f8d6a5fb86f69b420a9222454539b76256308
Joey Biotti <joey@MacBook-Pro-2.local>
2017-08-08 10:41:05 -0500

    db seeds now

--- src/Bangazon/data/DBInitializer.cs
+++ src/Bangazon/data/DBInitializer.cs
@@ -2,7 +2,7 @@ using System;
 using Bangazon.Models;
 using Microsoft.Data.Sqlite; 
 
-namespace Bangazon.Data
+namespace Bangazon
 {
     public static class DbInitializer
     {
@@ -18,25 +18,25 @@ namespace Bangazon.Data
                 if(customersExist == false)
                 {
                     dab.BulkInsert($@"
-                        insert into customer values(1, 'Bobby', 'Schmurda', 'Rykers Island', 'Brooklyn', 'NY', '12345', '555-555-5555');
-                        insert into customer values(2, 'Lil', 'Pump', '2 Main St.', 'Miami', 'FL', '23456', '555-555-5551');
-                        insert into customer values(3, 'Smoke', 'Purpp', '3 Main St.', 'Miami', 'FL', '34567', '555-555-5552');
+                        insert into customer values(null, 'Bobby', 'Schmurda', 'Rykers Island', 'Brooklyn', 'NY', '12345', '555-555-5555');
+                        insert into customer values(null, 'Lil', 'Pump', '2 Main St.', 'Miami', 'FL', '23456', '555-555-5551');
+                        insert into customer values(null, 'Smoke', 'Purpp', '3 Main St.', 'Miami', 'FL', '34567', '555-555-5552');
 
-                        insert into order values(null, 1);
-                        insert into order values(null, 2);
-                        insert into order values(null, 3);
+                        insert into paymentType values(null, 12, 'Cash', 1);
+                        insert into paymentType values(null, 13, 'Visa', 2);
+                        insert into paymentType values(null, 14, 'MasterCard', 3);
 
-                        insert into paymentType values(null, 'Cash', 1, 12);
-                        insert into paymentType values(null, 'Visa', 2, 13);
-                        insert into paymentType values(null, 'MasterCard', 3, 14);
+                        insert into [order] values (null, 1, 1);
+                        insert into [order] values(null, 2, 2);
+                        insert into [order] values(null, 3, 3);
 
-                        instert into product(null, 'Codine Cough Syrup', 3, 12.99, 1, 'Goes great with Sprite!');
-                        instert into product(null, 'SoundCloud Subscription', 2, 9.99, 2, 'Where all the good hip hop is at');
-                        instert into product(null, 'Soccer Ball', 1, 10.99, 3, 'Kick goals with it!');
+                        insert into product values (null, 'Codine Cough Syrup', 3, 12.99, 1, 'Goes great with Sprite!');
+                        insert into product values (null, 'SoundCloud Subscription', 2, 9.99, 2, 'Where all the good hip hop is at');
+                        insert into product values (null, 'Soccer Ball', 1, 10.99, 3, 'Kick goals with it!');
 
-                        insert into productOrder(null, 1, 2);
-                        insert into productOrder(null, 2, 1);
-                        insert into productOrder(null, 3, 3);
+                        insert into productOrder values (null, 1, 2);
+                        insert into productOrder values (null, 2, 1);
+                        insert into productOrder values (null, 3, 3);
                     ");
                 }
             });
