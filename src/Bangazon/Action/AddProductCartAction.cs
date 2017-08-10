ffa688e0914109367d9fb6faf27b4548c081177e
cleverbek1991 <dilshod@Dilshods-MacBook-Pro.local>
2017-08-10 14:03:26 -0500

    Addorder

--- src/Bangazon/Action/AddProductCartAction.cs
+++ src/Bangazon/Action/AddProductCartAction.cs
@@ -6,12 +6,30 @@ namespace Bangazon.Actions
 {
     public class AddProductCart
     {
-        public static void DoAction(OrderManager ord, ProductManager prod)
+        /*
+        Class:   AddProductCart
+        Purpose: method that is called from Program.cs switch case: 5
+                 accepts 3 arguments
+                 first argument is an Instance of the Class ProductManager
+                 second argument is an Instance of the Class OrderManager
+                 third is the CustomerId that was selected and stored from switch case: 2
+                 Assigns the product to the productOrder table
+        Author:  Dilshod
+        */
+        public static void DoAction(OrderManager om, ProductManager pm, int custId)
         {
+            // Get list of Products
+            var products = pm.GetProductList();
+            var counter = 1;
             Console.Clear();
-            Console.WriteLine ("Order");
-
-            
+            Console.WriteLine ("Choose a product to add to your cart");
+            foreach (var product in products)
+                {
+                    Console.WriteLine($"{counter++}. {product.Name}");
+                }
+            Console.WriteLine($"Press {counter} to quit");
+            Console.Write ("> ");
+            int chooseCustomer = int.Parse(Console.ReadLine());
         }
     }
 }
